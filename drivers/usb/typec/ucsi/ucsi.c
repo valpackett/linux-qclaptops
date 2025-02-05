28794b03e55306c4a7becb220fc773a7a86fb9aa
Abel Vesa <abel.vesa@linaro.org>
2025-02-05 13:45:05 +0200

    usb: typec: ucsi: Schedule connector worker on freezable workqueue
    Currently, the UCSI connector worker is scheduled on the non-freezable
    system workqueue. During system suspend, on a plug/unplug event, the
    worker can run before the devices have actually resumed. The UCSI
    instances can implement operations that might need to do some HW accesses
    while the devices are still suspended.
    
    Scheduling the USCI connector worker on the freezable system workqueue
    instead will ensure the devices are resumed by the time the worker is
    scheduled to run.
    
    Link: https://lore.kernel.org/r/20250205-ucsi-schedule-conn-worker-on-freezable-wq-v1-1-107d1356b77b@linaro.org
    Signed-off-by: Abel Vesa <abel.vesa@linaro.org>

--- drivers/usb/typec/ucsi/ucsi.c
+++ drivers/usb/typec/ucsi/ucsi.c
@@ -1575,7 +1575,7 @@ void ucsi_connector_change(struct ucsi *ucsi, u8 num)
 	con = &ucsi->connector[num - 1];
 
 	if (!test_and_set_bit(EVENT_PENDING, &ucsi->flags))
-		schedule_work(&con->work);
+		queue_work(system_freezable_wq, &con->work);
 }
 EXPORT_SYMBOL_GPL(ucsi_connector_change);
 
