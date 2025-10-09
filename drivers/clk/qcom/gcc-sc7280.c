f31a2b440ba09794cea51c8679f1666d2d176083
Xilin Wu <sophon@radxa.com>
2025-10-09 18:29:16 +0800

    clk: qcom: gcc-sc7280: Allow PCIe_0 GDSC to enter retention state
    This allows suspend and resume to work properly on this platform.
    
    Signed-off-by: Xilin Wu <sophon@radxa.com>

--- drivers/clk/qcom/gcc-sc7280.c
+++ drivers/clk/qcom/gcc-sc7280.c
@@ -3098,7 +3098,7 @@ static struct gdsc gcc_pcie_0_gdsc = {
 	.pd = {
 		.name = "gcc_pcie_0_gdsc",
 	},
-	.pwrsts = PWRSTS_OFF_ON,
+	.pwrsts = PWRSTS_RET_ON,
 	.flags = VOTABLE | RETAIN_FF_ENABLE,
 };
 
