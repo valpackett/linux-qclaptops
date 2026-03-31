6b7690be60941ecb074c6dc9ced15a56ca7b7ac6
Jiali Chen <chenjiali@radxa.com>
2026-03-31 10:34:30 +0000

    drm/msm/dsi: enable continuous clock for SC7280 DSI PHY
    Add set_continuous_clock callback to the 7nm 7280 PHY
    configuration, enabling continuous DSI clock mode support.
    
    Signed-off-by: Jiali Chen <chenjiali@radxa.com>

--- drivers/gpu/drm/msm/dsi/phy/dsi_phy_7nm.c
+++ drivers/gpu/drm/msm/dsi/phy/dsi_phy_7nm.c
@@ -1346,6 +1346,7 @@ const struct msm_dsi_phy_cfg dsi_phy_7nm_7280_cfgs = {
 		.pll_init = dsi_pll_7nm_init,
 		.save_pll_state = dsi_7nm_pll_save_state,
 		.restore_pll_state = dsi_7nm_pll_restore_state,
+		.set_continuous_clock = dsi_7nm_set_continuous_clock,
 	},
 	.min_pll_rate = 600000000UL,
 #ifdef CONFIG_64BIT
