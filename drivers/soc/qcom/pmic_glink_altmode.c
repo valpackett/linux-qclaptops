19d34c917cbbbc73e7d7753ce35ed2deeb0ca732
Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>
2026-06-08 00:33:08 +0300

    soc: qcom: pmic-glink-altmode: pass down HPD_IRQ events
    The DisplayPort standard defines a special kind of HPD events called
    IRQ_HPD. These events are used to notify DP Source about the events on
    the Sink side.
    
    Pass IRQ_HPD events from the firmware to the HPD bridge, letting those
    to be delivered to the DisplayPort driver.
    
    Reviewed-by: Konrad Dybcio <konrad.dybcio@oss.qualcomm.com>
    Acked-by: Bjorn Andersson <andersson@kernel.org>
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>

--- drivers/soc/qcom/pmic_glink_altmode.c
+++ drivers/soc/qcom/pmic_glink_altmode.c
@@ -374,7 +374,11 @@ static void pmic_glink_altmode_worker(struct work_struct *work)
 		else
 			conn_status = connector_status_disconnected;
 
-		drm_aux_hpd_bridge_notify(&alt_port->bridge->dev, conn_status);
+		drm_aux_hpd_bridge_notify_extra(&alt_port->bridge->dev,
+						conn_status,
+						alt_port->hpd_irq ?
+						DRM_CONNECTOR_DP_IRQ_HPD :
+						DRM_CONNECTOR_NO_EXTRA_STATUS);
 	} else if (alt_port->mux_ctrl == MUX_CTRL_STATE_TUNNELING) {
 		if (alt_port->svid == USB_TYPEC_TBT_SID)
 			pmic_glink_altmode_enable_tbt(altmode, alt_port);
