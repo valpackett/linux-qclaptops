66baf93828e6b26685a367ec222cd26960d4fbb6
Val Packett <val@packett.cool>
2026-08-03 01:43:12 -0300

    clk: qcom: dispcc-sc7280: Stop disp_cc_mdss_mdp_clk_src from getting parked
    If the clock frequency is changed at registration time, a flicker will be
    visible at boot.
    Switching to clk_rcg2_shared_no_init_park_ops fixes this.
    
    Signed-off-by: Val Packett <val@packett.cool>

--- drivers/clk/qcom/dispcc-sc7280.c
+++ drivers/clk/qcom/dispcc-sc7280.c
@@ -307,7 +307,7 @@ static struct clk_rcg2 disp_cc_mdss_mdp_clk_src = {
 		.name = "disp_cc_mdss_mdp_clk_src",
 		.parent_data = disp_cc_parent_data_4,
 		.num_parents = ARRAY_SIZE(disp_cc_parent_data_4),
-		.ops = &clk_rcg2_shared_ops,
+		.ops = &clk_rcg2_shared_no_init_park_ops,
 	},
 };
 
